f73301b6768ccc859d71ca347798080c1250747c
danielkuciej <biuro.symmetry@gmail.com>
2024-05-23 22:29:05 +0200

    Selected buttons

--- irregular_verbs.js
+++ irregular_verbs.js
@@ -1,1 +1,6 @@
 'use strict';
+
+// Selecting elements
+const btnNewWord = document.querySelector('.btn-new');
+const btnCheck = document.querySelector('.btn-check');
+const btnReset = document.querySelector('.btn-reset');
