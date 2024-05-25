dcfbcc72deb722cabc8f8dd1d430f60792d15cc0
danielkuciej <biuro.symmetry@gmail.com>
2024-05-25 13:01:49 +0200

    Created data arrays

--- irregular_verbs.js
+++ irregular_verbs.js
@@ -4,3 +4,56 @@
 const btnNewWord = document.querySelector('.btn-new');
 const btnCheck = document.querySelector('.btn-check');
 const btnReset = document.querySelector('.btn-reset');
+
+//Datas
+const translationWords = [
+  'być',
+  'bić',
+  'urodzić się',
+  'zostawać',
+  'zaczynać',
+  'łamać/psuć się',
+  'przynosić',
+  'budować',
+  'kupować',
+  'łapać',
+];
+
+const infinitiveWords = [
+  'be',
+  'beat',
+  'be born',
+  'become',
+  'begin',
+  'break',
+  'bring',
+  'build',
+  'buy',
+  'catch',
+];
+
+const pastSimpleWords = [
+  'was/were',
+  'beat',
+  'was/were born',
+  'became',
+  'began',
+  'broke',
+  'brought',
+  'built',
+  'bought',
+  'caught',
+];
+
+const pastParticipleWords = [
+  'been',
+  'beaten',
+  'have/has been born',
+  'become',
+  'begun',
+  'broken',
+  'brought',
+  'built',
+  'bought',
+  'caught',
+];
